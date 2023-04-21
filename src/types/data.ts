b3480411fc910339ae18951f819389aeeb565d7a
bsw98 <qwqq6954@naver.com>
2023-04-21 16:27:00 +0900

    setting: data type 지정

---
+++ src/types/data.ts
@@ -0,0 +1,38 @@
+interface User {
+  uid: string;
+  name: string;
+  nickname: string;
+  email: string;
+  post_uid: string[] | null;
+  introduce: string | null;
+  profile_url: string | null;
+  phone: string | null;
+  followers: string[] | null;
+  following: string[] | null;
+  scrap: string[] | null;
+}
+
+interface Comment {
+  user_uid: string;
+  content: string;
+  createAt: string;
+  like: string[];
+  recomment: Recomment[];
+}
+
+interface Recomment {
+  user_uid: string;
+  content: string;
+  createAt: string;
+  like: string[];
+}
+
+interface Post {
+  uid: string;
+  user_uid: string;
+  content: string;
+  createAt: string;
+  images: string[];
+  like: string[];
+  comment: Comment[];
+}
