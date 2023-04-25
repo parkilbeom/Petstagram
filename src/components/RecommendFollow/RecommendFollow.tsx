d448fab7416afe72ae38f069f0e1355484258302
Yesong Choi <to06109@naver.com>
2023-04-26 03:54:30 +0900

    feat: 팔로우 추천 컴포넌트 마크업 완료(#27)

--- src/components/RecommendFollow/RecommendFollow.tsx
+++ src/components/RecommendFollow/RecommendFollow.tsx
@@ -1,32 +1,69 @@
-import { FollowList } from '@/components';
+import { FollowList, UserList } from '@/components';
+import Link from 'next/link';
+import styled from 'styled-components';
 
 export function RecommendFollow() {
+  const userData = {
+    email: 'to06109@naver.com',
+    profile_url:
+      'https://dimg.donga.com/wps/NEWS/IMAGE/2022/01/28/111500268.2.jpg',
+    nickname: '멍뭉이0',
+  };
   const testImageObject = [
-    {
-      email: 'to06109@naver.com',
-      profile_url:
-        'https://dimg.donga.com/wps/NEWS/IMAGE/2022/01/28/111500268.2.jpg',
-      nickname: '멍뭉이1',
-    },
     {
       email: 'bsw@naver.com',
       profile_url:
         'https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/32E9/image/BA2Qyx3O2oTyEOsXe2ZtE8cRqGk.JPG',
-      nickname: '멍뭉이2',
+      nickname: '멍뭉이1',
     },
     {
       email: 'yesong@naver.com',
       profile_url:
         'https://interbalance.org/wp-content/uploads/2021/08/flouffy-VBkIK3qj3QE-unsplash-scaled-e1631077364762.jpg',
-      nickname: '멍뭉이3',
+      nickname: '멍뭉이2',
+    },
+    {
+      email: 'bsw@naver.com',
+      profile_url:
+        'https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/32E9/image/BA2Qyx3O2oTyEOsXe2ZtE8cRqGk.JPG',
+      nickname: '멍뭉이1',
+    },
+    {
+      email: 'yesong@naver.com',
+      profile_url:
+        'https://interbalance.org/wp-content/uploads/2021/08/flouffy-VBkIK3qj3QE-unsplash-scaled-e1631077364762.jpg',
+      nickname: '멍뭉이2',
+    },
+    {
+      email: 'bsw@naver.com',
+      profile_url:
+        'https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/32E9/image/BA2Qyx3O2oTyEOsXe2ZtE8cRqGk.JPG',
+      nickname: '멍뭉이1',
     },
   ];
 
   return (
-    <>
+    <FollowArticle>
+      <UserList profile={userData} />
+      <TitleBox>
+        <h2>회원님을 위한 추천</h2>
+        <Link href='/main'>모두 보기</Link>
+      </TitleBox>
       {testImageObject.map((profile, index) => {
         return <FollowList key={index} profile={profile} />;
       })}
-    </>
+    </FollowArticle>
   );
 }
+
+const FollowArticle = styled.section`
+  width: 420px;
+  padding: 34px 25px 20px 25px;
+`;
+
+const TitleBox = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  margin-bottom: 25px;
+`;
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,11 +1,30 @@
 import { PostCard, RecommendFollow } from '@/components';
+import styled from 'styled-components';
 
 export default function Main() {
   return (
-    <>
-      <PostCard />
-      <PostCard />
+    <MainContainer>
+      <MenuSection>메뉴바</MenuSection>
+      <PostSection>
+        <PostCard />
+        <PostCard />
+      </PostSection>
       <RecommendFollow />
-    </>
+    </MainContainer>
   );
 }
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const MenuSection = styled.section`
+  background-color: beige;
+  width: 20%;
+`;
+
+const PostSection = styled.section`
+  display: flex;
+  flex-flow: column nowrap;
+`;
